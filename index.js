2cdecfe0d72ccd91e8723f891a56683b7265b93b
anna-marie-vos <amvos@outlook.com>
2017-02-01 12:11:01 +1300

    access and authentication with cookies works

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express()
 const session = require('express-session')
 const bodyParser = require('body-parser')
 const path = require('path')
+const users = require('./table')
+const _ = require('lodash')
+
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -18,9 +21,43 @@ app.get('/', function (req, res, next) {
   res.sendFile(path.join(__dirname, 'form.html'))
 })
 
-app.get('/welcome', function(req, res, next){
-  const messages = 'Hello '+req.session.name
+app.post('/login', function(req, res, next){
+const user = _.find(users.table, {name: req.body.name});
+  if(!user){
+    res.send('user not found')
+  } else if(user.password === req.body.password){
+    req.session.activeUser = user.name
+    req.session.adminRights = user.admin
+    res.send('logged in')
+  } else{
+    res.send('invalid password')
+  }
+})
+
+function checkLoginDetails(req,res,next){
+  const user = req.session.activeUser
+    if(!user){
+      res.send('access denied ')
+    }
+      next()
+}
+
+app.get('/private',checkLoginDetails,function(req,res,next){
+  const messages = 'you are '+req.session.activeUser
   res.send(messages)
+})
+
+function checkAdminRights(req,res,next){
+  const adminRights = req.session.adminRights
+    if(adminRights){
+      next()
+    } else {
+      res.send("you're not the one")
+    }
+}
+
+app.get('/admin',checkAdminRights,function(req,res,next){
+  res.send("you've got rights")
 })
 
 app.get('/logout', function(req,res,next){
---
+++ table.js
@@ -0,0 +1,6 @@
+module.exports={
+  table:[
+    {name:'bilbo',password:'qwe',admin:false},
+    {name:'frodo',password:'asd',admin:true},
+  ]
+}
