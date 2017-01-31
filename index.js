5dd07a0591a57c3750341b50c5b3899bd2ea3032
anna-marie-vos <amvos@outlook.com>
2017-01-31 16:09:12 +1300

    cookie is storingstuff

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 var express = require('express')
 var app = express()
+var session = require('express-session')
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}))
+
+app.use(function(req,res,next){
+  req.session.name = 'Garfield'
+  next()
 })
+
+app.get('/', function (req, res, next) {
+  // console.log('req.headers', req.headers);
+  res.send(req.session.name)
+})
+
+app.get('/welcome', function(req, res, next){
+  const messages = 'Hello '+req.session.name
+  res.send(messages)
+})
+
+
+
 
 app.listen(3000)
